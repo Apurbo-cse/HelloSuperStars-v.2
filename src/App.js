6bb339334f5772493054a43045330da13be9bf7a
Apurbo Kumar Anup <apurboka@gmail.com>
2023-01-12 14:12:04 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -10,29 +10,31 @@ import Home from "./components/pages/Home";
 import Eshowcase from "./components/pages/e-showcase/Eshowcase";
 import { useTheme } from "./components/context.js/ThemeContext";
 import LeftSidebar from "./components/layout/include/LeftSidebar";
+import RightSidebar from "./components/layout/include/RightSidebar";
 
 function App() {
   const { isDarkMode } = useTheme();
   return (
-    <div className={`main-position ${isDarkMode ? "lightMode" : "darkMode"}`}>
+    <div className={`post-main-body ${isDarkMode ? "lightMode" : "darkMode"}`}>
       <Navigation />
 
-      <div className="left-position ">
-        <LeftSidebar />
-      </div>
-      <div className="container">
-        <div className="row pt-3">
-          <div className="col-lg-3"></div>
+      <div className="container-fluid">
+        <div className="row pt-3 d-flex justify-content-between">
+          <div className="col-lg-3 left-position">
+            {" "}
+            <LeftSidebar />
+          </div>
           <div className="col-sm-12 col-md-12 col-lg-6 post-main">
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/e-showcase" element={<Eshowcase />} />
             </Routes>
           </div>
-          <div className="col-lg-3"></div>
+          <div className="col-lg-3 right-position">
+            <RightSidebar />
+          </div>
         </div>
       </div>
-      <div className="right-position bg-dark">dsfdsfsdf</div>
       <Footer />
     </div>
   );
--- src/components/layout/include/LeftSidebar.js
+++ src/components/layout/include/LeftSidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LeftCard from "../../common/sidebar/LeftCard";
 import imagePath from "../../constants/imagePath";
 import { useTheme } from "../../context.js/ThemeContext";
 
@@ -7,157 +8,16 @@ const LeftSidebar = () => {
   return (
     <>
       <div className={`post mb-3 p-1 ${isDarkMode ? null : "dark-post"} `}>
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.category} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Category</h6>
-            <small
-              className={`left-font-sub ${
-                isDarkMode ? "left-sub-text" : "gold-color"
-              } `}
-            >
-              5 Selected
-            </small>
-          </div>
-        </div>
+        
+        <LeftCard title={"Category"} icon={imagePath.category} count={10} />
+        <LeftCard title={"Following"} icon={imagePath.following} count={10} />
+        <LeftCard title={"Fan Group"} icon={imagePath.fangroup} count={10} />
+        <LeftCard title={"Enroll Auditions"} icon={imagePath.enroll} count={10} />
+        <LeftCard title={"Activities"} icon={imagePath.activities} count={10} />
+        <LeftCard title={"Settings"} icon={imagePath.setting} />
+        <LeftCard title={"Terms & Policy"} icon={imagePath.term}  />
 
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.following} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Following</h6>
-            <small
-              className={`left-font-sub ${
-                isDarkMode ? "left-sub-text" : "gold-color"
-              } `}
-            >
-              4 Superstars
-            </small>
-          </div>
-        </div>
 
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.fangroup} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Fan Group</h6>
-            <small
-              className={`left-font-sub ${
-                isDarkMode ? "left-sub-text" : "gold-color"
-              } `}
-            >
-              00 Group
-            </small>
-          </div>
-        </div>
-
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.package} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Packages</h6>
-            <small
-              className={`left-font-sub ${
-                isDarkMode ? "left-sub-text" : "gold-color"
-              } `}
-            >
-              00 Star
-            </small>
-          </div>
-        </div>
-
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.enroll} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Enroll Auditions</h6>
-            <small
-              className={`left-font-sub ${
-                isDarkMode ? "left-sub-text" : "gold-color"
-              } `}
-            >
-              10 activities
-            </small>
-          </div>
-        </div>
-
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.activities} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Activities</h6>
-            <small
-              className={`left-font-sub ${
-                isDarkMode ? "left-sub-text" : "gold-color"
-              } `}
-            >
-              10 activities
-            </small>
-          </div>
-        </div>
-
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.setting} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Settings</h6>
-            {/* <small className={`left-font-sub ${isDarkMode ? "left-sub-text" : "gold-color"} `}>
-              10 activities
-            </small> */}
-          </div>
-        </div>
-
-        <div
-          className={` d-flex align-items-center  m-2 ${
-            isDarkMode ? "left-side bd-shadow " : "left-side-dark"
-          } `}
-        >
-          <div>
-            <img src={imagePath.setting} alt="" className="left-icon p-2" />
-          </div>
-          <div>
-            <h6 className="p-0 m-0">Terms & Policy</h6>
-            {/* <small className={`left-font-sub ${isDarkMode ? "left-sub-text" : "gold-color"} `}>
-              10 activities
-            </small> */}
-          </div>
-        </div>
       </div>
     </>
   );
